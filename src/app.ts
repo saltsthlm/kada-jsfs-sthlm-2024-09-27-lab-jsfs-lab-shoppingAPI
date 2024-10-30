7414761f8a892cc51ee9324c2b92e5c4ee4859d3
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-30 13:47:46 +0100

    add-route-for-api-products-id
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
 let counter = 1;
-app.use("/", async (req: Request, res: Response) => {
+app.get("/api/products/", async (req: Request, res: Response) => {
   try {
     let fileArr = [];
     const files = await readdir(products.db);
@@ -22,6 +22,29 @@ app.use("/", async (req: Request, res: Response) => {
     }
 
     res.json(fileArr);
+  } catch (err) {
+    console.error(err);
+  }
+});
+
+app.get("/api/products/:id", async (req: Request, res: Response) => {
+
+  const paramsId = req.params.id;
+  console.log(paramsId);
+
+  try {
+    let fileArr = [];
+    const files = await readdir(products.db);
+    for (const file of files) {
+      const filePath = path.join(products.db, file);
+      const fileContent = await readFile(filePath, "utf-8");
+
+      fileArr.push(JSON.parse(fileContent));
+    }
+
+    const product = fileArr.filter((product) => product.id === paramsId)
+
+    res.json(product);
   } catch (err) {
     console.error(err);
   }
