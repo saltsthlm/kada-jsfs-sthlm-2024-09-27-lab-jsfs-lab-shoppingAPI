43faeaab599d189fa16f88986bd096c2e714ba85
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-30 15:43:52 +0100

    add-products-to-file
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -4,7 +4,7 @@ import config from "./config";
 const { products, carts } = config;
 import { readdir, readFile, writeFile, appendFile } from "fs/promises";
 import type { Cart, Product } from "./__tests__/e2e/e2e-types";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 import { validate } from "./cart/api/validation";
 
 const app = express();
@@ -31,12 +31,11 @@ app.get("/api/products/", async (req: Request, res: Response) => {
 });
 
 app.get("/api/products/:id", async (req: Request, res: Response) => {
-
   const paramsId = req.params.id;
   console.log(paramsId);
 
   try {
-    let fileArr:Product[] = [];
+    let fileArr: Product[] = [];
     const files = await readdir(products.db);
     for (const file of files) {
       const filePath = path.join(products.db, file);
@@ -45,8 +44,8 @@ app.get("/api/products/:id", async (req: Request, res: Response) => {
       fileArr.push(JSON.parse(fileContent));
     }
 
-    const product = fileArr.find((product) => product.id === paramsId)
-    if(typeof product === 'undefined') {
+    const product = fileArr.find((product) => product.id === paramsId);
+    if (typeof product === "undefined") {
       res.status(404).json("No product with this id: " + paramsId);
     }
     res.json(product);
@@ -56,70 +55,53 @@ app.get("/api/products/:id", async (req: Request, res: Response) => {
 });
 
 app.post("/api/carts/", (req: Request, res: Response) => {
-    const id = uuidv4();
+  const id = uuidv4();
 
-    const cart: Cart = {
-        id,
-        products: []
-    };
+  const cart: Cart = {
+    id,
+    products: [],
+  };
 
-    writeFile(path.join(carts.db, id), JSON.stringify(cart.products));    
+  writeFile(path.join(carts.db, id), JSON.stringify(cart.products));
 
-    res.status(201).location(`/api/carts/${id}`).json(cart);
+  res.status(201).location(`/api/carts/${id}`).json(cart);
 });
 
 app.patch("/api/carts/:id", async (req: Request, res: Response) => {
-    const cartId = req.params.id;
-    console.log("CArt id", cartId)
-    
-    const product = validate(req.body);
+  const product = validate(req.body);
 
+  if (!product.success) {
+    res.status(400).json({ error: "Invalid request" });
+  } else {
+    const cartPath = path.join(carts.db, req.params.id);
 
-    if (!product.success) {
-        res.status(400).json({error: "Invalid request"});
-    } else {
-        await appendFile(path.join(carts.db, cartId), JSON.stringify(product.data), 'utf8');
+    const products = JSON.parse(await readFile(cartPath, "utf-8")) as Product[];
+    products.push(product.data);
+    await writeFile(cartPath, JSON.stringify(products), "utf8");
 
-        res.status(204).json("Added product to carrt: " + product.data.id);
-    }    
+    res.status(204).json("Added product to cart: " + product.data.id);
+  }
 });
 
 app.get("/api/carts/:id", async (req: Request, res: Response) => {
-    try {
-        const cart: Cart = [];
-       
-          const filePath = path.join(carts.db, req.params.id);
-          const products = await readFile(filePath, "utf-8");
-          
+  try {
+    const filePath = path.join(carts.db, req.params.id);
+    const products = JSON.parse(await readFile(filePath, "utf-8")) as Product[];
 
-    
-          fileArr.push(JSON.parse(fileContent));
-        
-    
-        res.json(fileArr);
-      } catch (err) {
-        console.error(err);
-      }
-    const cartId = req.params.id;
-    console.log("CArt id", cartId)
-    
-    const product = validate(req.body);
+    const cart: Cart = {
+      id: req.params.id,
+      products,
+    };
 
-
-    if (!product.success) {
-        res.status(400).json({error: "Invalid request"});
-    } else {
-        await appendFile(path.join(carts.db, cartId), JSON.stringify(product.data), 'utf8');
-
-        res.status(204).json("Added product to carrt: " + product.data.id);
-    }    
+    res.json(cart);
+  } catch (err) {
+    console.error(err);
+  }
 });
 
-
 app.use((req: Request, res: Response) => {
-    res.status(404).json({error: "Not found"});
-})
-
+  res.status(404).json({ error: "Not found" });
+});
 
 // Add your own middlware here!
 
