4cab5ddb3f19d95582be378e993ed32d1b1bd3c7
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-30 11:49:57 +0100

    new-json-bodyparser
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,10 @@
 import express from 'express';
 import path from 'path';
-import bodyParser from 'body-parser';
 
 const app = express();
 
 // Some nice middleware :)
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Add your own middlware here!
