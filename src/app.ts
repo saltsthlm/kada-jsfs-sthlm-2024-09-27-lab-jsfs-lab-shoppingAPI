f5c75f33ad36c502f9d0ac3d14a83fc9c4ca3934
Anton Berglund <antonberglund_92@hotmail.com>
2024-10-30 12:12:49 +0100

    updated-to-tsx
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Carl <carl@wbsweden.com>

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,18 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import path from 'path';
-
+import config from './config';
 const app = express();
 
 // Some nice middleware :)
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', (req: Request, res: Response) => {
+
+    const productsPath = config.products.db;
+    res.send(productsPath);
+
+})
 // Add your own middlware here!
 
 export default app;
