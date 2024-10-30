8167ecf05b9740357da33ea366b4aaa9abdbfd9e
CarlUdo <carl@wbsweden.com>
2024-10-30 11:38:24 +0100

    Update-packages
    Co-authored-by: Leo <leo@swederus.se>
    Co-authored-by: Anton <antonberglund_92@hotmail.com>

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts}"]},
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    rules: {
+      semi: "warn",
+      eqeqeq: "warn",
+      "no-unused-vars": "warn",
+      "prefer-const": "warn",
+    },
+  },
+];
